699fa50257feaaa8092aa699596062a2ef4d36aa
surajpaib <surajballambat@gmail.com>
2017-12-06 17:04:27 +0530

    Add Factory

---
+++ Factory/Factory.py
@@ -0,0 +1,17 @@
+import abc
+
+
+class Shape:
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def getCircle(self):
+        pass
+
+    @abc.abstractmethod
+    def getCircle(self):
+        pass
+
+
+class ShapeFinder(ShapeObject):
+    pass
